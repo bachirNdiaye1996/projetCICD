992a9f948e0cc231fcf78159f04e6ec3757efc2f
Bachir <mouhamadoulbachir2@gmail.com>
2023-09-12 11:41:17 +0200

    Premier commit

--- src/main/java/com/example/demo/ProjetCicdApplication.java
+++ src/main/java/com/example/demo/ProjetCicdApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjetCicdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -15,6 +19,11 @@ public class ProjetCicdApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(ProjetCicdApplication.class);
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "Demo de CICD Application";
 	}
 
 }
